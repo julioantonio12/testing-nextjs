472e01a34eda204202a4d6e6c4a4a7ccecb0d53c
Julio González Martín <jagonzalez@getinsoft.com>
2021-03-23 16:24:49 +0100

    Add my first page with layout where I present myself

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -8,12 +8,15 @@ export default function FirstPost() {
       <Head>
         <title>First Post</title>
       </Head>
-      <h1>First Post</h1>
+      <h1>First Page!</h1>
+      <p>Hello, this is my first page made with <b>Nextjs</b>. In this course I'll try to do my best to learn all that I can and I'm gonna practice doing a project on my job. The project's name is: <b>Praxis</b>.</p>
+      <p>Link to my <Link href="https://github.com/julioantonio12/testing-nextjs"><a>Github</a></Link></p>
       <h2>
         <Link href="/">
           <a>Back to home</a>
         </Link>
       </h2>
+
     </Layout>
   )
 }

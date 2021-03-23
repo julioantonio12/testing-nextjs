bc029f152fe38713b5127c75738428d5edac76a1
Julio González Martín <jagonzalez@getinsoft.com>
2021-03-23 20:54:06 +0100

    Render MD files with remark library

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,9 +1,8 @@
 import Layout from '../../components/layout'
-// import { getAllPostIds, getPostData } from '../../lib/posts'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id)
+  const postData = await getPostData(params.id)
   return {
     props: {
       postData
@@ -27,6 +26,8 @@ export default function Post({ postData }) {
       {postData.id}
       <br />
       {postData.date}
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
   )
 }
